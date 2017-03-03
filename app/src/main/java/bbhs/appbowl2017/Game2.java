f41f896dd4712206b139876e5dd4f485a1e8f754
nfusionz <owenxie24@gmail.com>
2017-03-03 09:39:45 -0500

    Create Game2.java for future edits today

---
+++ app/src/main/java/bbhs/appbowl2017/Game2.java
@@ -0,0 +1,17 @@
+package bbhs.appbowl2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Game2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
