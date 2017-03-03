e4d7ceb3efa0a70595559bce29a6374a3831b3a7
oxie <owenxie24@gmail.com>
2017-03-03 15:30:23 -0500

    ButtonsClicked methods (done by Adam)

--- app/src/main/java/bbhs/appbowl2017/Game2.java
+++ app/src/main/java/bbhs/appbowl2017/Game2.java
@@ -1,20 +1,46 @@
 package bbhs.appbowl2017;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import javax.xml.datatype.Duration;
+
 public class Game2 extends AppCompatActivity {
+
+    public FloatingActionButton settings;
+    public FloatingActionButton play;
+    public LinearLayout layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game2);
-
+        settings = (FloatingActionButton) findViewById(R.id.game2_settings);
+        play = (FloatingActionButton) findViewById(R.id.game2_play);
+        layout = (LinearLayout) findViewById(R.id.game2InfoLayout);
+        buttonsClicked();
+    }
+    public void buttonsClicked(){
+        play.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Snackbar snackbar =  Snackbar.make(layout, "Play clicked!", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Snackbar snackbar =  Snackbar.make(layout, "Settings clicked!", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
     }
 
-    
 
 }
