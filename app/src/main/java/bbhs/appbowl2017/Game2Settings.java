5c26b8dfff47dbe0906b9eb2dd367cdd7d1b437c
Nfusionz <owenxie24@gmail.com>
2017-03-07 09:18:22 -0500

    Added game2settings.java

---
+++ app/src/main/java/bbhs/appbowl2017/Game2Settings.java
@@ -0,0 +1,12 @@
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game2Settings{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_game2settings);//activity_game2settings.xml doesn't exist
+    }
+    
+}
