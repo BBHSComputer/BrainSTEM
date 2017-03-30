47e08e43463fb16abb355e934c9c07412769e59b
Nfusionz <owenxie24@gmail.com>
2017-03-30 08:54:33 -0400

    Work on game2_settings

--- app/src/main/java/bbhs/appbowl2017/Game2Settings.java
+++ app/src/main/java/bbhs/appbowl2017/Game2Settings.java
@@ -2,13 +2,29 @@ package  bbhs.appbowl2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Game2Settings extends AppCompatActivity{
     
+	private Button home;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_game2settings);//activity_game2settings.xml doesn't exist
+        //setContentView(R.layout.activity_game2settings);//activity_game2settings.xml doesn't exist yet
+		
+		//home = (Button) getViewById(R.id.game2_settingHome);//TODO add button game2_settingHome
+		
+		buttonsClicked();
     }
+
+	private void buttonsClicked(){
+		home.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+			 startActivity(new Intent(getApplicationContext, MainActivity.class));
+			}
+		});
+	}
     
 }
