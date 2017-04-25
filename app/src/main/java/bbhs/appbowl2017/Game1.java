fc9504483484d88ace150333541903559c8dc303
Joe-Wilson <ratratus50@gmail.com>
2017-04-25 07:33:03 -0400

    Update Game1.java

--- app/src/main/java/bbhs/appbowl2017/Game1.java
+++ app/src/main/java/bbhs/appbowl2017/Game1.java
@@ -6,7 +6,7 @@ import android.net.Uri;
  * Created by joewilson on 2/20/2017.
  */
 public class Game1 {
-public static  Uri[] cards;
+public static  Uri[] cards; //Store card images
     public static int test;
 
     public static void makeFinal(){
