6b7acaec7f8e9aa0489f8246d13e98fe3d711950
SwissAndOr <2000matu@gmail.com>
2017-04-26 19:04:46 -0400

    More Game4 updates

--- app/src/main/java/bbhs/appbowl2017/Game4.java
+++ app/src/main/java/bbhs/appbowl2017/Game4.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static bbhs.appbowl2017.R.drawable.parking;
@@ -28,12 +29,13 @@ public class Game4 extends AppCompatActivity implements QuestionListener {
         setContentView(R.layout.activity_game4);
 
         // Setup questions
-        Log.d("Image", "" + getResources().getIdentifier("parking" , "drawable", getPackageName()));
         questions.add(new SimpleQuestion(this, "On which parking space is the car parked?", new String[]{"78", "89", "87", "97"}, 2, getResources().getIdentifier("parking" , "drawable", getPackageName())));
         questions.add(new SimpleQuestion(this, "A and C but not B", new String[]{"A: A", "B: B, A, and D", "C: C and D", "D: D and A"}, 2));
         questions.add(new SimpleQuestion(this, "Where is I?", new String[]{"Here", "There", "Hair", "Anywhere"}, 2));
         questions.add(new SimpleQuestion(this, "Choose the answer closest to the correct answer", new String[]{"the answer", "the answer", "the answer", "the answer"}, 0));
         questions.add(new BiggestQuestion(this, "Click on the biggest answer", new String[]{"Answer!", "Answer.", "Big", "Answer?"}));
+        questions.add(new TimedQuestion(this, "Run out of time", new String[]{"Done", "Run", "No", "What?"}, 0, 5000));
+        Collections.shuffle(questions);
 
         playButton = (Button) findViewById(R.id.playButton);
 
