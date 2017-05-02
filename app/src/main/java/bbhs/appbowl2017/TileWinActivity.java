d7f966adeb68c7a90f493df28872aa7b78d7cba1
Adam <adam@adamfly.me>
2017-05-01 22:46:03 -0400

    Saving score for Summation, Music, and Tile completed! Just waiting on Stack.

--- app/src/main/java/bbhs/appbowl2017/TileWinActivity.java
+++ app/src/main/java/bbhs/appbowl2017/TileWinActivity.java
@@ -1,6 +1,7 @@
 package bbhs.appbowl2017;
 
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class TileWinActivity extends AppCompatActivity {
     public static double score, length, moves;
     private TextView tScore,tTime,tMoves;
     private Button mainMenu,playAgain;
+    public static double fTime, fMoves, fScore; //F meaning final, post Weiss' algorithms
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +49,27 @@ public class TileWinActivity extends AppCompatActivity {
         tTime.setText(tTime.getText().toString() + minutes + ":" + seconds);
         tMoves.setText(tMoves.getText().toString() + moves);
         tScore.setText(tScore.getText().toString() + (int)(score*-100));
+        fTime = minutes + seconds;
+        fMoves = moves;
+        fScore = score*-100;
+        int savedTime = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getInt("tileTime", Integer.MAX_VALUE);
+        if (savedTime > fTime){
+            PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().putInt("tileTime", (int)fTime).commit();
+            savedTime = (int)fTime;
+        }
+        int savedMoves = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getInt("tileMoves", Integer.MAX_VALUE);
+        if (savedMoves > fMoves){
+            PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().putInt("tileMoves", (int)fMoves).commit();
+            savedMoves = (int)fMoves;
+        }
+        int savedScore = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getInt("tileScore", 0);
+        if (savedScore < fScore){
+            PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().putInt("tileScore", (int)fScore).commit();
+            savedScore = (int)fScore;
+        }
+        tTime.setText(tTime.getText().toString() + "\nBest: " + savedTime);
+        tMoves.setText(tMoves.getText().toString() + "\nBest: " + savedMoves);
+        tScore.setText(tScore.getText().toString() + "\nBest: " + savedScore);
 
 
 
