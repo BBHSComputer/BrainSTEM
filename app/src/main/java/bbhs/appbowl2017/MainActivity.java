e5d2b1574a4199c571eaa5d531b15b034fb53fd8
ricky3350 <ricky3350@users.noreply.github.com>
2017-05-09 21:44:18 -0400

    Music layout changes and other stuff

--- app/src/main/java/bbhs/appbowl2017/MainActivity.java
+++ app/src/main/java/bbhs/appbowl2017/MainActivity.java
@@ -1,13 +1,15 @@
 package bbhs.appbowl2017;
 
 import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
