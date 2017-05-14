79fed0388e1e34a7aef97140e4f1cc0ea9997d62
ricky3350 <ricky3350@users.noreply.github.com>
2017-05-14 15:36:19 -0400

    Added runtime permissions necessary for API >= 23

--- app/src/main/java/bbhs/appbowl2017/tile/ChooseImagesDialog.java
+++ app/src/main/java/bbhs/appbowl2017/tile/ChooseImagesDialog.java
@@ -1,5 +1,6 @@
 package bbhs.appbowl2017.tile;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -7,12 +8,15 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -119,11 +123,22 @@ public class ChooseImagesDialog extends DialogFragment {
 		customImages.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				for (Integer ix : selectedIndices) {
-					currentlyUsedDefaultImages.remove(ix);
-					getImage(ix);
+				if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+					// Should we show an explanation?
+					if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+						// TODO (from tutorial: https://developer.android.com/training/permissions/requesting.html)?
+						// Show an explanation to the user *asynchronously* -- don't block
+						// this thread waiting for the user's response! After the user
+						// sees the explanation, try again to request the permission.
+					} else {
+						// No explanation needed, we can request the permission.
+						ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 11610);
+						// 11610 is an app-defined int constant. The callback method gets the result of the request.
+					}
+				} else {
+					getCustomImages();
 				}
-				selectedIndices.clear();
 			}
 		});
 
@@ -181,6 +196,34 @@ public class ChooseImagesDialog extends DialogFragment {
         }
 
     }
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case 11610:
+				Log.d("PERMISSIONS", "RESPONSE"); // TODO: This is never called; it probably should go in the MainActivity class, but the MainActivity class cannot access the getCustomImages() method.
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted, yay! Do the
+					// task you need to do.
+					getCustomImages();
+				} else {
+					// permission denied, boo! Disable the
+					// functionality that depends on this permission.
+				}
+				break;
+			// other 'case' lines to check for other
+			// permissions this app might request
+		}
+	}
+
+	private void getCustomImages() {
+		for (Integer ix : selectedIndices) {
+			currentlyUsedDefaultImages.remove(ix);
+			getImage(ix);
+		}
+		selectedIndices.clear();
+	}
 
     private Uri uriForDrawable(int resId) {
 		Resources resources = getContext().getResources();
